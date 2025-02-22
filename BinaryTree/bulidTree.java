75bddbe44b71aa9d556d4ca01062c45b979b53de
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2025-02-22 19:38:36 +0530

    SuppressWarnings

--- BinaryTree/bulidTree.java
+++ BinaryTree/bulidTree.java
@@ -81,6 +81,7 @@ public class bulidTree{
             }
         }
     }
+    @SuppressWarnings("static-access")
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
     
