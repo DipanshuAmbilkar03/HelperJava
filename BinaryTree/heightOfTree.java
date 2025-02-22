cfc856bce85c95e21556ce7059dfa23410b447d8
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2025-02-22 19:39:09 +0530

    SuppressWarnings - resources

--- BinaryTree/heightOfTree.java
+++ BinaryTree/heightOfTree.java
@@ -37,6 +37,7 @@ public class heightOfTree {
     }
     public static void main(String[] args) {
         System.out.println("enter root node : ");
+        @SuppressWarnings("resource")
         Scanner sc = new Scanner(System.in);
         int data = sc.nextInt();
         Node root = new Node(data);
