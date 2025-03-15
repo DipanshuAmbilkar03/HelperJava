7db296a9f69889cc61badd23d0d66c52a0898388
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2025-03-15 19:34:19 +0530

    stack using arraylist

--- Stack/Implementation.java
+++ Stack/Implementation.java
@@ -1,6 +1,25 @@
 import java.util.ArrayList;
 
-public class Implementation{    
+public class Implementation{  
+    static class stack2 {
+        static ArrayList<Integer> s = new ArrayList<>(); 
+        public static void push_(int data) {
+            s.add(data);
+        }
+
+        public static boolean isEmpty_() {
+            return s.size() == 0;
+        }
+
+        public static void pop_() {
+            s.remove(s.get(s.size()-1));
+        }
+
+        public static int peek_() {
+            return s.get(s.size()-1);
+        }
+    }
+    
     static class Stack {
         static ArrayList<Integer> list = new ArrayList<>() ;
         public static boolean isEmpty() {
@@ -29,6 +48,16 @@ public class Implementation{
         while(!s.isEmpty()) {
             System.out.println(s.peek());
             s.pop();
+        }
+
+        stack2 s2 = new stack2();
+        s2.push_(1);
+        s2.push_(2);
+        s2.push_(3);
+
+        while(!s2.isEmpty_()) {
+            System.out.println(s2.peek_());
+            s2.pop_();
         }
     }
 }
