a2bee263f2395ddd3d5f4bb65fb78442d64d3d8e
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2025-03-16 12:25:10 +0530

    next greatest number in a series (Optimized using Stack). Time complexity is O(n) not O(n^2) the while loop perform stack operation in which elements get inside stack and outside which means O(2n) and for loop's complexity = O(n). Therefore total comlexity = n + 2n => O(3n) --> O(n)

---
+++ 26Stack/findNextGreatestNum/StacknextGreatestNum.java
@@ -0,0 +1,34 @@
+package findNextGreatestNum;
+
+import java.util.Stack;
+
+public class StacknextGreatestNum {
+    public static void main(String[] args) {
+        int[] arr = {6,8,0,1,3};
+        int[] nextGreater = new int[arr.length];
+        Stack<Integer> s = new Stack<>();
+
+        for(int i=arr.length-1; i>=0; i--) {
+
+            // while
+            while(!s.isEmpty() && arr[s.peek()] <= arr[i]) {
+                s.pop();
+            }
+
+            // if-else
+                if(s.isEmpty()) {
+                    nextGreater[i] = -1;
+                } else {
+                    nextGreater[i] = arr[s.peek()];
+                }
+
+            // push el to the stack
+
+            s.push(i);
+        }
+
+        for(int i=0; i<nextGreater.length; i++) {
+            System.out.print(nextGreater[i]+" ");
+        }
+    }
+}
