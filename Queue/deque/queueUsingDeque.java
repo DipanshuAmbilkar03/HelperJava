9906209628a0dd9c176ce1f0624296f00363ebe8
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2025-03-24 19:30:16 +0530

    implementation queue using deque

---
+++ Queue/deque/queueUsingDeque.java
@@ -0,0 +1,34 @@
+package Queue.deque;
+
+import java.util.LinkedList;
+import java.util.Deque;
+
+public class queueUsingDeque {
+    static class queue {
+        Deque<Integer> deque = new LinkedList<>();
+        
+        public void add(int data) {
+            deque.addLast(data);
+        }
+
+        public int remove() {
+            return deque.removeFirst();
+        }
+
+        public int peek() {
+            return deque.getFirst();
+        }
+    }
+    public static void main(String[] args) {
+        queue q = new queue();
+
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        System.out.println("peek -> " + q.peek())   ;
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+    }
+}
