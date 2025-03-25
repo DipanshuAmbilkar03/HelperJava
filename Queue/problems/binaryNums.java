7b4e476bc3f5715ece1925b2a60cf1f52be0b509
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2025-03-25 20:13:21 +0530

    binary number using queue

---
+++ Queue/problems/binaryNums.java
@@ -0,0 +1,26 @@
+package Queue.problems;
+
+import java.util.*;
+
+public class binaryNums {
+    public static void binarynum(int num) {
+        Stack s = new Stack<>();
+
+        for(int i=1; i<=num; i++) {
+            int n = i;
+            while (n > 0) {
+                s.push(n % 2);
+                n = n / 2;
+            }
+
+            while(!s.isEmpty()) {
+                System.out.print(s.peek() + " ");
+                s.pop();
+            }
+        }   
+    }
+    public static void main(String[] args) {
+        int opt = 5;
+        binarynum(opt);
+    }
+}
