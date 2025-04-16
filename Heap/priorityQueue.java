6148f4dc347209f20fcc52b059a6d41a490a2a1b
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2025-04-16 19:20:30 +0530

    priority Queue

---
+++ Heap/priorityQueue.java
@@ -0,0 +1,18 @@
+package Heap;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class priorityQueue {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(10);
+        pq.add(1);
+        pq.add(21);
+        pq.add(32);
+        
+        while(!pq.isEmpty()) {
+            System.out.println(pq.peek());
+            pq.remove();
+        }
+    }
+}
