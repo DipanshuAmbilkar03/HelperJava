df0b198843158e04b7434970c25c4636cf669f40
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2025-05-15 20:09:33 +0530

    knapsack using tabulation

---
+++ DP/0_1_knapsack/knap_tabulation.java
@@ -0,0 +1,49 @@
+public class knap_tabulation {
+    // tabulation using iteration
+    public static int knap(int[] val,int[] wt,int w) {
+        int n = val.length;
+        int dp[][] = new int[n+1][w+1];
+
+        // initialization of 0th row as every value be 0
+        for(int i=0; i<dp.length;i++) {
+            dp[i][0] = 0;
+        }
+
+        // initialization of 0th coloum as every value be 0
+        for(int j=0; j<dp[0].length;j++) {
+            dp[0][j] = 0;
+        }
+
+        for(int i=1;i<n+1;i++) {
+            for(int j=1;j<w+1;j++) {
+                int v = val[i-1];
+                int W = wt[i-1];
+
+                if(W <= j) {
+                    // valid 
+
+                    // include the value
+                    int includeProfit = v + dp[i-1][j-W];
+                    // exclude the value
+                    int excludeProfit = dp[i-1][j];
+
+                    dp[i][j] = Math.max(includeProfit, excludeProfit);
+                }else {
+                    // invalid 
+                    int excludeProfit = dp[i-1][j];
+                    dp[i][j] = excludeProfit;
+                }
+            }
+        }
+
+        return dp[n][w];
+    }   
+
+    public static void main(String[] args) {
+        int[] val = {15,14,10,45,30};
+        int[] wt = {2,5,1,3,4};
+        int w = 7;
+    
+        System.out.println("Maximum value obtained from knapSack is (tabulation): "+knap(val, wt, w));
+    }
+}
