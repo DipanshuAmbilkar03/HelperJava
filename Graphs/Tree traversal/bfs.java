91e45cc8366093845323c7721811ff1adf7e52c6
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2025-05-22 20:11:44 +0530

    BFS

--- Graphs/Tree traversal/bfs.java
+++ Graphs/Tree traversal/bfs.java
@@ -53,13 +53,12 @@ public class bfs {
         graph[3].add(new Edge(3, 1, 1));
         graph[3].add(new Edge(3, 4, 1));
         graph[3].add(new Edge(3, 5, 1));
-        
+
         // 4 - vertex
         graph[4].add(new Edge(4, 2, 1));
         graph[4].add(new Edge(4, 3, 1));
         graph[4].add(new Edge(4, 5, 1));
 
-        
         graph[5].add(new Edge(5, 3, 1));
         graph[5].add(new Edge(5, 4, 1));
         graph[5].add(new Edge(5, 6, 1));
