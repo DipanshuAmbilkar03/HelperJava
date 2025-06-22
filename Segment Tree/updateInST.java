23c2183726c867f698df16cd165fdfde73a7131d
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2025-06-22 19:50:48 +0530

    Construction of a segment tree and then appling update method in it.

---
+++ Segment Tree/updateInST.java
@@ -0,0 +1,88 @@
+// package Segment Tree;
+
+public class updateInST {
+    static int tree[];
+
+    public static void init(int n) {
+        tree = new int[4*n];
+    }   
+
+    public static int st(int arr[],int i,int start,int end) {
+        if(start == end) {
+            tree[i] = arr[start];
+            return arr[start];
+        }
+
+        int mid = (start + end) / 2;
+
+        st(arr, 2*i+1, start, mid);
+        st(arr, 2*i+2, mid+1, end);
+        tree[i] = tree[2*i+1] + tree[2*i+2];
+
+        return tree[i];
+    }
+    public static void printTree() {
+        for(int i=0; i<tree.length; i++) {
+            System.out.print(tree[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static int getUtil(int i,int si,int sj,int qi,int qj) {
+        if(qj <= si || qi >= sj) {
+            return 0; //non overlap condition
+        } else if (qi <= si && qj >= sj) {
+            return tree[i]; // completely overlap condition 
+        }else {
+            int mid = (si + sj) / 2;
+            int left = getUtil(2*i+1, si, mid, qi, qj);
+            int right = getUtil(2*i+2, mid+1, sj, qi, qj);
+        
+            return left + right;
+        }
+    }
+
+    public static int getSum(int arr[],int qi,int qj) {
+        int n = arr.length;
+        return getUtil(0, 0, n-1, qi, qj);
+    }
+
+    public static void updateUtil(int i,int si,int sj, int idx,int diff) {
+        if(idx > sj || idx < si) { //non overlapping case 
+            return;
+        }
+
+        tree[i] += diff;
+
+        // all other cases 
+        if(si != sj) {
+            int mid = (si + sj) / 2;
+            updateUtil(2*i+1, si, mid, idx, diff);
+            updateUtil(2*i+2, mid+1, sj, idx, diff);
+        }
+    }
+
+    public static void update(int arr[],int idx, int newVal) {
+        int n = arr.length;
+        int diff = newVal - arr[idx];
+        arr[idx] = newVal;
+
+        updateUtil(0, 0, n-1, idx, diff);
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {1,2,3,4,5,6,7,8};
+        int n = arr.length;
+
+        init(n);
+        st(arr, 0, 0, n-1);
+
+        // printTree();
+
+        System.out.println(getSum(arr, 2, 5));
+        update(arr, 2, 2);
+        System.out.println(getSum(arr, 2, 5));
+
+    }
+}
+
