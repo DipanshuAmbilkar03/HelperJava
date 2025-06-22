5177ba642a308745e37e8ca8bc8dcfcd9f899b03
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2025-06-22 19:50:06 +0530

    Construction of a segment tree.

---
+++ Segment Tree/segmentTree.java
@@ -0,0 +1,38 @@
+// package Segment Tree;
+
+public class segmentTree {
+    static int tree[];
+
+    public static void init(int n) {
+        tree = new int[4*n];
+    }   
+
+    public static int st(int arr[],int i,int start,int end) {
+        if(start == end) {
+            tree[i] = arr[start];
+            return arr[start];
+        }
+
+        int mid = (start + end) / 2;
+
+        st(arr, 2*i+1, start, mid);
+        st(arr, 2*i+2, mid+1, end);
+        tree[i] = tree[2*i+1] + tree[2*i+2];
+
+        return tree[i];
+    }
+    public static void printTree() {
+        for(int i=0; i<tree.length; i++) {
+            System.out.print(tree[i] + " ");
+        }
+    }
+    public static void main(String args[]) {
+        int arr[] = {1,2,3,4,5,6,7,8};
+        int n = arr.length;
+
+        init(n);
+        st(arr, 0, 0, n-1);
+
+        printTree();
+    }
+}
