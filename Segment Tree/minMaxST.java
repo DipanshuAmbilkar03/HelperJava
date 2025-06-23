a539827d3a395202f367b608bf8762b75e3d032c
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2025-06-23 11:39:54 +0530

    Minimum maximum in Segment tree.

---
+++ Segment Tree/minMaxST.java
@@ -0,0 +1,34 @@
+public class minMaxST {
+    static int tree[];
+
+    public static void init(int n) {
+        tree = new int[4*n];
+    }
+
+    public static void builtTree(int i,int arr[], int si, int sj) {
+        if (si == sj) {
+            tree[i] = arr[si];
+            return;
+        }
+
+        int mid = (si + sj) / 2;
+        builtTree(2*i+1,arr, si, mid);
+        builtTree(2*i+2,arr, mid+1, sj);
+
+        tree[i] = Math.max(tree[2*i+1], tree[2*i+2]);
+    }
+
+
+    public static void main(String[] args) {
+        int arr[] = {6,8,-1,2,17,1,3,2,4};
+        int n = arr.length;
+
+        init(n);
+        builtTree(0, arr, 0, n-1);
+
+        for(int i=0; i<tree.length; i++) {
+            System.out.print(tree[i] + " ");
+        }
+        System.out.println();
+    }
+}
