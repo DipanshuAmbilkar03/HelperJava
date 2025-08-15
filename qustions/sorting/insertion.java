542090ca8c1d003e054b30720b6cf8e3daf5ed0b
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2025-08-15 20:35:49 +0530

    inserton sort

---
+++ qustions/sorting/insertion.java
@@ -0,0 +1,23 @@
+public class insertion {
+    public static void insertion(int[] arr,int n) {
+        for(int i=1;i<n;i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while(j >=0 && arr[j] > arr[key]) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+
+            arr[j+1] = key;
+        }
+
+        for(int i=0;i<n;i++) {
+            System.out.print(arr[i]+"->");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {10,2,5,1,15};
+        int n = arr.length;
+        insertion(arr,n);
+    }
+}
