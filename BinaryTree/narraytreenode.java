4a01e6d4b575388882ed08c5142cf43862ffcdfd
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2026-01-27 23:04:59 +0530

    narray tree

--- BinaryTree/narraytreenode.java
+++ BinaryTree/narraytreenode.java
@@ -1,64 +1,66 @@
 package BinaryTree;
 
-import java.util.ArrayList;
-import java.util.Queue;
+import java.util.*;
 
 public class narraytreenode {
-	static class NArrayTreeNode<E> {
-		E data;
-		ArrayList<Integer> children;
-		
-//		public NArrayTreeNode(E data) {
-//			this.data = data;
-//			children = new ArrayList<>();
-//		}
-		
-		public NArrayTreeNode(E data,ArrayList<Integer> children) {
-			this.data = data;
-			this.children = children;
-		}
-	}
-	
-	public ArrayList<Integer> bfs(NArrayTreeNode<Integer> root) {
-		ArrayList<Integer> res = new ArrayList<Integer>(); 
-			
-		Queue<NArrayTreeNode> q = new LinkedList<>();
-		q.add(root);
-		
-		while(!q.isEmpty()) {
-			NArrayTreeNode<Integer> node = q.poll();
-			res.add(node.data);
-			for(NArrayTreeNode child : node.children) {
-				if(child!=null) {
-					q.offer(child);
-				}
-			}
-		}
-		return res;
-	}
-	
-    public static void main(String[] args) {
-    	NArrayTreeNode<Integer> root = new NArrayTreeNode<>(1);
-    	
-    	NArrayTreeNode<Integer> node2 = new NArrayTreeNode<>(2);
-    	NArrayTreeNode<Integer> node3 = new NArrayTreeNode<>(3);
-    	NArrayTreeNode<Integer> node4 = new NArrayTreeNode<>(4);
-  
-    	NArrayTreeNode<Integer> node5 = new NArrayTreeNode<>(5);
-    	NArrayTreeNode<Integer> node6 = new NArrayTreeNode<>(6);
-    	
-    	root.children(node2);
-    	root.children(node3);
-    	root.children(node4);
+    static class NArrayTreeNode<E> {
+        E data;
+        ArrayList<NArrayTreeNode<E>> children;
 
-    	node3.children(node5);
-    	node3.children(node6);
-    	
-    	
-    	ArrayList<Integer> bfsTraversal = bfs(root);
-    	System.out.println("BFS Traversal:");
+		// public NArrayTreeNode(E data,ArrayList<NArayTreeNode> children) {
+		// 	this.data = data;
+		// 	this.children = children;
+		// }
+
+        public NArrayTreeNode(E data) {
+            
+            this.data = data;
+            this.children = new ArrayList<>();
+        }
+    }
+
+    public static ArrayList<Integer> bfs(NArrayTreeNode<Integer> root) {
+
+        ArrayList<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+
+        Queue<NArrayTreeNode<Integer>> q = new LinkedList<>();
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+            NArrayTreeNode<Integer> node = q.poll();
+            res.add(node.data);
+
+            for (NArrayTreeNode<Integer> child : node.children) {
+                if (child != null) {
+                    q.offer(child);
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        NArrayTreeNode<Integer> root = new NArrayTreeNode<>(1);
+
+        NArrayTreeNode<Integer> node2 = new NArrayTreeNode<>(2);
+        NArrayTreeNode<Integer> node3 = new NArrayTreeNode<>(3);
+        NArrayTreeNode<Integer> node4 = new NArrayTreeNode<>(4);
+        NArrayTreeNode<Integer> node5 = new NArrayTreeNode<>(5);
+        NArrayTreeNode<Integer> node6 = new NArrayTreeNode<>(6);
+
+        root.children.add(node2);
+        root.children.add(node3);
+        root.children.add(node4);
+
+        node3.children.add(node5);
+        node3.children.add(node6);
+
+        ArrayList<Integer> bfsTraversal = bfs(root);
+
+        System.out.println("BFS Traversal:");
         for (int val : bfsTraversal) {
             System.out.print(val + " ");
         }
-    }	
+    }
 }
