cd771597e70efb3cae2e5120dec9b982aa1b410e
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2026-02-19 12:09:50 +0530

    GFG : Floor in BST

---
+++ LastRun2/DP/CountNodesBinaryTree.java
@@ -0,0 +1,55 @@
+package LastRun2.DP;
+import java.util.*;
+
+public class CountNodesBinaryTree {
+
+    // Binary Tree Node
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static int countNodes(TreeNode root) {
+
+        if (root == null) return 0;
+
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+
+    public static void main(String[] args) {
+
+        /*
+                1
+               / \
+              2   3
+             / \
+            4   5
+        */
+
+        TreeNode root = new TreeNode(
+                1,
+                new TreeNode(
+                        2,
+                        new TreeNode(4),
+                        new TreeNode(5)
+                ),
+                new TreeNode(3)
+        );
+
+        int result = countNodes(root);
+        System.out.println("Total Nodes: " + result);
+    }
+}
