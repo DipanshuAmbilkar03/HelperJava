fb91eb9f153d3893bc717153962599f67cf0cd6e
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2026-06-17 01:27:27 +0530

    We consider two strings to be anagrams of each other if the first string's letters can be rearranged to form the second string. In other words, both strings must contain the same exact letters in the same exact frequency. For example, bacdc and dcbac are anagrams, but bacdc and dcbad are not.
    Alice is taking a cryptography class and finding anagrams to be very useful. She decides on an encryption scheme involving two large strings where encryption is dependent on the minimum number of character deletions required to make the two strings anagrams. Can you help her find this number?
    
    Given two strings,  and , that may not be of the same length, determine the minimum number of character deletions required to make  and  anagrams. Any characters can be deleted from either of the strings.
    
    Example.
    
    The only characters that match are the 's so we have to remove  from  and  from  for a total of  deletions.
    
    Function Description
    
    Complete the makingAnagrams function in the editor below.
    
    makingAnagrams has the following parameter(s):
    
    string s1: a string
    string s2: a string
    Returns
    
    int: the minimum number of deletions needed
    Input Format
    
    The first line contains a single string, .
    The second line contains a single string, .
    
    Constraints
    
    It is guaranteed that  and  consist of lowercase English letters, ascii[a-z].

---
+++ HackerRank/Strings/Solution.java
@@ -0,0 +1,66 @@
+package HackerRank.Strings;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'makingAnagrams_' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING s1
+     *  2. STRING s2
+     */
+
+    public static int makingAnagrams(String s1, String s2) {
+    // Write your code here
+        int[] freq = new int[26];
+
+        for (char ch : s1.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        for (char ch : s2.toCharArray()) {
+            freq[ch - 'a']--;
+        }
+
+        int deletions = 0;
+
+        for (int count : freq) {
+            deletions += Math.abs(count);
+        }
+
+        return deletions;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s1 = bufferedReader.readLine();
+
+        String s2 = bufferedReader.readLine();
+
+        int result = Result.makingAnagrams(s1, s2);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
