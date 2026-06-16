5efe5c16d82675b1ac852f8ebcf5106d5364d375
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2026-06-17 01:24:31 +0530

    Given a string of lowercase letters in the range ascii[a-z], determine the index of a character that can be removed to make the string a palindrome. There may be more than one solution, but any will do. If the word is already a palindrome or there is no solution, return -1. Otherwise, return the index of a character to remove.
    Example
    
    Either remove 'b' at index  or 'c' at index .
    
    Function Description
    
    Complete the palindromeIndex function in the editor below.
    
    palindromeIndex has the following parameter(s):
    
    string s: a string to analyze
    Returns
    
    int: the index of the character to remove or
    Input Format
    
    The first line contains an integer , the number of queries.
    Each of the next  lines contains a query string .
    
    Constraints
    
    All characters are in the range ascii[a-z].
    Sample Input
    
    STDIN   Function
    -----   --------
    3       q = 3
    aaab    s = 'aaab' (first query)
    baa     s = 'baa'  (second query)
    aaa     s = 'aaa'  (third query)
    Sample Output
    
    3
    0
    -1
    Explanation
    
    Query 1: "aaab"
    Removing 'b' at index  results in a palindrome, so return .
    
    Query 2: "baa"
    Removing 'b' at index  results in a palindrome, so return .
    
    Query 3: "aaa"
    This string is already a palindrome, so return . Removing any one of the characters would result in a palindrome, but this test comes first.
    
    Note: The custom checker logic for this challenge is available here.

---
+++ HackerRank/Strings/palindromeIndex_.java
@@ -0,0 +1,84 @@
+package HackerRank.Strings;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'palindromeIndex' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int palindromeIndex(String s) {
+        int start = 0;
+        int end = s.length()-1;
+        if(s.length() == 1) return -1;
+        while(start < end) {
+            if(s.charAt(start) == s.charAt(end)) {
+                start++;
+                end--;
+            }
+            else {
+                if (isPalindrome(s, start + 1, end))
+                    return start;
+
+                if (isPalindrome(s, start, end - 1))
+                    return end;
+
+                return -1;
+            }
+        }
+        
+        return -1;
+    }
+    
+    private static boolean isPalindrome(String s, int t, int e) {
+        while(t<e) {
+            if(s.charAt(t) != s.charAt(e)) {
+                return false;
+            }
+            
+            t++;
+            e--;
+        }
+        
+        return true;
+    }
+}
+
+public class palindromeIndex_ {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                int result = Result.palindromeIndex(s);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
