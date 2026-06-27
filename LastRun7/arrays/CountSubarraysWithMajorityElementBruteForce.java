f22fc58d3dfb6fc495adc7ad3f8126798da44af1
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2026-06-28 01:17:16 +0530

    3020. Find the Maximum Number of Elements in Subset (Medium) You are given an array of positive integers nums.
    You need to select a subset of nums which satisfies the following condition:
    
    You can place the selected elements in a 0-indexed array such that it follows the pattern: [x, x2, x4, ..., xk/2, xk, xk/2, ..., x4, x2, x] (Note that k can be be any non-negative power of 2). For example, [2, 4, 16, 4, 2] and [3, 9, 3] follow the pattern while [2, 4, 8, 4, 2] does not.
    Return the maximum number of elements in a subset that satisfies these conditions.
    
    Example 1:
    
    Input: nums = [5,4,1,2,2]
    Output: 3
    Explanation: We can select the subset {4,2,2}, which can be placed in the array as [2,4,2] which follows the pattern and 22 == 4. Hence the answer is 3.
    Example 2:
    
    Input: nums = [1,3,2,4]
    Output: 1
    Explanation: We can select the subset {1}, which can be placed in the array as [1] which follows the pattern. Hence the answer is 1. Note that we could have also selected the subsets {2}, {3}, or {4}, there may be multiple subsets which provide the same answer.
    
    Constraints:
    2 <= nums.length <= 105
    1 <= nums[i] <= 109

---
+++ LastRun7/arrays/CountSubarraysWithMajorityElementBruteForce.java
@@ -0,0 +1,44 @@
+package LastRun7.arrays;
+
+public class CountSubarraysWithMajorityElementBruteForce {
+
+    public static int countMajoritySubarrays(int[] nums, int target) {
+
+        int answer = 0;
+        int n = nums.length;
+
+        for (int start = 0; start < n; start++) {
+
+            int count = 0;
+
+            for (int end = start; end < n; end++) {
+
+                if (nums[end] == target) {
+                    count++;
+                }
+
+                if (count > (end - start + 1) / 2) {
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums1 = {1, 3, 2, 3, 3};
+        int target1 = 3;
+
+        int[] nums2 = {2, 1, 2, 1};
+        int target2 = 2;
+
+        int[] nums3 = {5, 5, 5};
+        int target3 = 5;
+
+        System.out.println(countMajoritySubarrays(nums1, target1));
+        System.out.println(countMajoritySubarrays(nums2, target2));
+        System.out.println(countMajoritySubarrays(nums3, target3));
+    }
+}
