f2c51d4190586c025a8a103b5d749e9b528617b9
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2026-07-20 20:56:19 +0530

    1260. Shift 2D Grid (Easy).
    Given a 2D grid of size m x n and an integer k. You need to shift the grid k times.
    
    In one shift operation:
    
    Element at grid[i][j] moves to grid[i][j + 1].
    Element at grid[i][n - 1] moves to grid[i + 1][0].
    Element at grid[m - 1][n - 1] moves to grid[0][0].
    Return the 2D grid after applying shift operation k times.

---
+++ LastRun7/Matrix Problems/SmallestSubsequenceOfDistinctCharacters.java
@@ -0,0 +1,62 @@
+package LastRun7.Matrix Problems;
+
+import java.util.Stack;
+
+public class SmallestSubsequenceOfDistinctCharacters {
+
+    public static String smallestSubsequence(String s) {
+
+        int[] lastOccurrence = new int[26];
+        boolean[] visited = new boolean[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            lastOccurrence[s.charAt(i) - 'a'] = i;
+        }
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            if (visited[ch - 'a']) {
+                continue;
+            }
+
+            while (!stack.isEmpty()
+                    && ch < stack.peek()
+                    && lastOccurrence[stack.peek() - 'a'] > i) {
+
+                visited[stack.pop() - 'a'] = false;
+            }
+
+            stack.push(ch);
+            visited[ch - 'a'] = true;
+        }
+
+        StringBuilder ans = new StringBuilder();
+
+        for (char ch : stack) {
+            ans.append(ch);
+        }
+
+        return ans.toString();
+    }
+
+    public static void main(String[] args) {
+
+        String[] testCases = {
+                "bcabc",
+                "cbacdcbc",
+                "cdadabcc",
+                "ecbacba",
+                "leetcode"
+        };
+
+        for (String s : testCases) {
+            System.out.println("Input : " + s);
+            System.out.println("Output: " + smallestSubsequence(s));
+            System.out.println();
+        }
+    }
+}
